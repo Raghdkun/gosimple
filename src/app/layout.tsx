91953bcadb4c40af5984903055b211785e6d0ee0
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2025-11-18 11:17:13 -0500

    feat: add Google Analytics tracking to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5K1GX51M8C"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5K1GX51M8C');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
